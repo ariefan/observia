2b76c59c18c6fd17959361a56040e27efd7c0682
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2025-09-02 22:05:02 +0700

    feat: add logging support in SettingController

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class SettingController extends Controller
