76bd8f31c671b523d7db23261e634f91cc1931d3
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2025-09-03 14:32:59 +0700

    style: update SVG icons in pedigree print template for improved visual representation

--- resources/views/certificate/pedigree-print.blade.php
+++ resources/views/certificate/pedigree-print.blade.php
@@ -47,7 +47,7 @@
             <div class="bg-white border-2 border-gray-600 rounded-lg p-2 w-48 h-24 flex items-center space-x-1 print:border-gray-700">
                 <div class="size-10 bg-blue-100 rounded-full flex-shrink-0 flex items-center justify-center print:bg-gray-100">
                     <svg class="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                     </svg>
                 </div>
                 <div class="flex-1 min-w-0 ml-1">
@@ -81,7 +81,7 @@
                                 <div class="bg-white border-2 border-gray-600 rounded-lg p-2 w-48 h-24 flex items-center space-x-1 print:border-gray-700">
                                     <div class="size-10 bg-blue-100 rounded-full flex-shrink-0 flex items-center justify-center print:bg-gray-100">
                                         <svg class="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                                         </svg>
                                     </div>
                                     <div class="flex-1 min-w-0 ml-1">
@@ -114,7 +114,7 @@
                                                 <div class="bg-white border-2 border-gray-600 rounded-lg p-2 w-48 h-24 flex items-center space-x-1 print:border-gray-700">
                                                     <div class="size-10 bg-blue-100 rounded-full flex-shrink-0 flex items-center justify-center print:bg-gray-100">
                                                         <svg class="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                                                         </svg>
                                                     </div>
                                                     <div class="flex-1 min-w-0 ml-1">
